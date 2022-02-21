0f476934005399d74a67f3958c042db78f8e7f03
Edgar RP <jefelitman@gmail.com>
2022-02-21 16:42:10 +0000

    Utils jobs v1.1

--- utils/jobs.py
+++ utils/jobs.py
@@ -1,16 +1,22 @@
 """This file contains code to process the jobs dictionary in the project
 and schedule objects.
 Created by: Edgar RP
-Version: 1.0.1
+Version: 1.1
 Job Dict Structure:
     {
         "id": Integer
         "mode": Integer
-        "duration": Integer
+        "base_duration": Integer
         "successors": List[Int]
         "renewable_resources_use": np.Array[Int]
         "nonrenewable_resources_use": np.Array[Int]
-        "doubly_constrained_use": np.Array[Int]
+        "doubly_constrained_use": np.Array[Int],
+        "init_random_duration": Integer,
+        "total_duration": Integer,
+        "risk_1": Float,
+        "risk_2": Float,
+        ...
+        "risk_n": Float
     }
 """
 
@@ -25,7 +31,7 @@ def __check_zero_params__(job):
     """
     zero_value = 0
     zero_params = [
-        "duration", 
+        "base_duration", 
         "renewable_resources_use", 
         "nonrenewable_resources_use", 
         "doubly_constrained_use"
@@ -33,7 +39,37 @@ def __check_zero_params__(job):
     for key in zero_params:
         zero_value += np.sum(job[key])
 
-    return zero_value == 0
+    return zero_value == 0 and job["mode"] == 1
+
+def get_job_modes(jobs, job_id):
+    """This function return all the modes for the given job id
+    and return a list with the modes.
+    Args:
+        jobs (List[Dict]): A list of dictionary of each job.
+        job_id (Integer): A number of which job will return its modes.
+    """
+    modes = []
+    for job in jobs:
+        if job["id"] == job_id:
+            modes.append(job["mode"])
+    if len(modes) == 0:
+        raise AssertionError("The job with id ({}) doesn't exist \
+            in the loaded project.".format(job_id))
+    else:        
+        return modes
+
+def get_job(jobs, job_id, mode):
+    """This function return the dict structure of the given 
+    job_id and mode.
+    Args:
+        jobs (List[Dict]): A list of dictionary of each job.
+        job_id (Integer): A number of which job will return its modes.
+    """
+    for job in jobs:
+        if job["id"] == job_id and job["mode"] == mode:
+            return job
+    raise AssertionError("The job with id ({}) and mode ({}) doesn't exist \
+        in the loaded project.".format(job_id, mode))
 
 def get_initial_job(jobs):
     """This function find the initial job in the list of job's 
@@ -44,15 +80,18 @@ def get_initial_job(jobs):
     for job in jobs:
         if __check_zero_params__(job) and len(job["successors"]) != 0:
             return job["id"]
+    raise ValueError("The project have an initial job?")
         
 def get_final_job(jobs):
     """This function find the initial job in the list of job's 
     dictionary and return its id.
     Args:
-        jobs (List[Dict]): A list of dictionary of each job."""
+        jobs (List[Dict]): A list of dictionary of each job.
+    """
     for job in jobs:
         if __check_zero_params__(job) and len(job["successors"]) == 0:
             return job["id"]
+    raise ValueError("The project have a final job?")
 
 def get_new_job_base_duration(duration, beta_generator):
     """This function return a new randomly duration for the duration given 
@@ -76,3 +115,33 @@ def get_new_job_base_duration(duration, beta_generator):
         return 1 + np.ceil(new_duration)
     else:
         return np.ceil(new_duration)
+
+def get_job_risks(base_duration, risks_per_job, beta_generator):
+    """This function will return a dictionary with the risk percentages 
+    and the new duration of the task with the risk applied. The dict will
+    have the following structure:
+    {
+        "risk_1": Float,
+        "risk_2": Float,
+        ...
+        "total_duration": Integer
+    }
+    Args:
+        base_duration (Integer): A value specifying the duration to apply risks.
+        risks_per_job (Tuple): Tuple of the percentages of happening that risk and
+        its length is the quantity of risks.
+        beta_generator (np.random.beta): Instance of numpy beta random value 
+        generator to get random values.
+    """
+    new_duration = base_duration
+    risks = {}
+    for i, p in enumerate(risks_per_job):
+        risk = "risk_{}".format(i+1)
+        if beta_generator() < p:
+            percentage = beta_generator()
+            risks[risk] = percentage
+            new_duration *= 1 + percentage
+        else:
+            risks[risk] = None
+    risks["total_duration"] = np.ceil(new_duration)
+    return risks
