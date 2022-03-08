ac12b06b37e9648e6695fdf0204848f71ab502c1
Edgar RP <jefelitman@gmail.com>
2022-03-08 20:40:26 +0000

    Solution v0.0.0

---
+++ solution.py
@@ -0,0 +1,4 @@
+"""This file contain the class that structure a solution for the project and estimates the base line, makespan and solution metrics as robust and quality of solution.
+Created by: Edgar RP
+Version: 0.0.0
+"""
