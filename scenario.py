95a2c6958c19e295f113ad11a10686cc0e8fccc0
Edgar RP <jefelitman@gmail.com>
2022-03-11 01:00:08 +0000

    Scenario v0.0.1

--- scenario.py
+++ scenario.py
@@ -1,4 +1,17 @@
 """This file contain the class that structure a scenario for the solution and also can create a base line for the solution.
 Created by: Edgar RP
-Version: 0.0.0
+Version: 0.0.1
 """
+
+import numpy as np
+import utils.jobs as uj
+
+class Scenario():
+    
+    def __init__(self, random_generator, jobs, renew_res, norenew_res, dou_res):
+        """When you create a Scenario for a Solution, it generate a list of activities to solve in time, calculate it own makespan and alter the base_duration for jobs. 
+        Args:
+            
+        """
+        
+        
