8272ebaf7c9bc5b1fd34413124d1b2b283d00ee8
Edgar RP <jefelitman@gmail.com>
2022-04-10 17:33:20 +0000

    Gen_Alg v1.2

--- gen_alg.py
+++ gen_alg.py
@@ -1,6 +1,6 @@
 """This file contain the class that execute and do the genetic algorithm of the given project with mutation, crossover and other methods neccessary.
 Created by: Edgar RP
-Version: 1.1
+Version: 1.2
 """
 
 import numpy as np
@@ -33,9 +33,7 @@ class Genetic_Algorithm():
         self.project = project
         self.initial_job = uj.get_initial_job(project["jobs"])
         self.final_job = uj.get_final_job(project["jobs"])
-        self.__set_job_list__(project["jobs"])
-        self.__set_jobs_dist_params__(project["jobs"], random_generator)
-        self.__set_jobs_risks__(n_jobs_risks, risks_per_job, random_generator)
+        self.__set_jobs_risks__(project["jobs"], n_jobs_risks, risks_per_job, random_generator)
         
         self.solutions = []
         for _ in range(poblation_size):
@@ -62,67 +60,55 @@ class Genetic_Algorithm():
         X_scaled = X_std * (new_max - new_min) + new_min
         return X_scaled
 
-    def __set_job_list__(self, jobs):
-        """This function set the dictionary of all the jobs in the project. This dictionary doesn't take into account the modes for the jobs but instead only focus in summarize the general parameters accross the scenearios like the risks and distribution params in the value elements.
+    def __set_jobs_risks__(self, jobs, n_jobs, risks_per, random_generator):
+        """This function set the params and risks for every job in the solution given the n_jobs with risks and risk_per job.
         Args:
             jobs (List[Dict]): A list of dictionary of each job.
-        """
-        self.jobs = {}
-        for job in jobs:
-            job_id = job["id"]
-            if job_id not in self.jobs.keys():
-                if job_id == self.initial_job:
-                    self.jobs[job_id] = {"initial": True}
-                elif job_id == self.final_job:
-                    self.jobs[job_id] = {"final": True}
-                else:
-                    self.jobs[job_id] = {}
-
-    def __set_jobs_dist_params__(self, jobs, random_generator):
-        """This function set the distribution params (mean and std) for all the jobs, excluding initial and final, to have common parameters to generate random values in its duration and risks.
-        Args:
-            jobs (List[Dict]): A list of dictionary of each job.
-            random_generator (scipy.stats.norm): Instance of scipy.stats.norm object to generate random values of the normal distribution.
-        """
-        prob = lambda: random_generator.cdf(random_generator.rvs())
-        for job_id in self.jobs:
-            if job_id not in [self.initial_job, self.final_job]:
-                modes = uj.get_job_modes_duration(jobs, job_id)
-                index = int(prob()*len(modes)) #Chose a random mode to use as base duration to generate the mean and std for all the jobs
-                duration = modes[list(modes.keys())[index]]
-                mean, std = uj.get_job_dist_params(duration, random_generator)
-                self.jobs[job_id]["normal_dist_mean"] = mean
-                self.jobs[job_id]["normal_dist_std"] = std
-
-    def __set_jobs_risks__(self, n_jobs, risks_per, random_generator):
-        """This function set the risk for every job in the solution given the n_jobs with risks and risk_per job.
-        Args:
             n_jobs (Integer): Quantity of jobs which will have risks.
             risks_per (Tuple): Tuple of the percentages of happening that risk and its length is the quantity of risks.
             random_generator (scipy.stats.norm): Instance of scipy.stats.norm object to generate random values of the normal distribution.
         """
-        prob = lambda: random_generator.cdf(random_generator.rvs())
+        self.jobs = {}
+        for job in jobs:
+            job_id = job["id"]
+            job_mode = job["mode"]
+            job_key = "{}.{}".format(job_id, job_mode)
+
+            if job_id == self.initial_job:
+                self.jobs[job_key] = {"initial": True}
+            elif job_id == self.final_job:
+                self.jobs[job_key] = {"final": True}
+            else:
+                self.jobs[job_key] = {
+                    "base_duration": job["base_duration"],
+                    "normal_dist_mean": job["base_duration"],
+                    "normal_dist_std": job["base_duration"] * 0.3
+                }
+
         jobs_modified = []
+        prob = lambda: random_generator.cdf(random_generator.rvs())
         while len(jobs_modified) < n_jobs:
             index = int(prob()*len(self.jobs))
-            job_id = list(self.jobs.keys())[index]
+            job_str = list(self.jobs.keys())[index]
+            job_id = int(job_str.split(".")[0])
             if job_id not in [self.initial_job, self.final_job] + jobs_modified:
-                mean = self.jobs[job_id]["normal_dist_mean"]
-                std = self.jobs[job_id]["normal_dist_std"]
-                risks = uj.get_job_risks(risks_per, mean, std)
+                risks = uj.get_job_risks(risks_per, self.jobs[job_str]["base_duration"])
                 is_none = True
                 for r in risks:
                     is_none = is_none and risks[r] == None
-                    self.jobs[job_id][r] = risks[r]
+                    for job_mode in uj.get_job_modes_duration(jobs, job_id):
+                        job_key = "{}.{}".format(job_id, job_mode)
+                        self.jobs[job_key][r] = risks[r]
                 if not is_none:
                     jobs_modified.append(job_id)
 
         # Let the remaining jobs with risks as None value
-        for job_id in self.jobs:
+        for job_str in self.jobs:
+            job_id = int(job_str.split(".")[0])
             if job_id not in [self.initial_job, self.final_job] + jobs_modified:
-                risks = uj.get_job_risks(np.zeros_like(risks_per), 0, 1)
+                risks = uj.get_job_risks(np.zeros_like(risks_per), 0)
                 for r in risks:
-                    self.jobs[job_id][r] = risks[r]
+                    self.jobs[job_str][r] = risks[r]
 
     def evolve_poblation(self, prob_ranges, n_cross_points, n_mutations, random_generator):
         """This function updates or make advance the initial poblation to the following generation, this method execute the crossover, mutation and sort solutions from best to worst to select who solutions will stay and which will be deleted.
