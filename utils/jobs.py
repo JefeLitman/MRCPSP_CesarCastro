e9cdee2510cf15815ab1e9c25584eed589d8f16b
Edgar RP <jefelitman@gmail.com>
2022-04-10 17:33:10 +0000

    Utils jobs v1.5

--- utils/jobs.py
+++ utils/jobs.py
@@ -1,6 +1,6 @@
 """This file contains code to process the jobs dictionary in the project and schedule objects.
 Created by: Edgar RP
-Version: 1.4.2
+Version: 1.5
 Job Dict Structure:
     {
         "id": Integer
@@ -40,15 +40,6 @@ def __check_zero_params__(job):
         zero_value += np.sum(job[key])
 
     return zero_value == 0 and job["mode"] == 1
-
-def __get_dist_prob__(mean, std):
-    """This function return the function callback to generate random probabilities from the normal distribution with mean and std given.
-    Args:
-        mean (Float): The mean of the normal distribution to use for the probabilities callback.
-        std (Float): The std of the normal distribution to use for the probabilities callback.
-    """
-    dist = stats.norm(loc=mean, scale=std)
-    return lambda: dist.cdf(dist.rvs())
 
 def get_job_modes_duration(jobs, job_id):
     """This function return all the duration for all modes of given job id and return a dictionary with keys as modes and values its durations.
@@ -98,36 +89,14 @@ def get_final_job(jobs):
             return job["id"]
     raise ValueError("The project have a final job?")
 
-def get_job_dist_params(duration, random_generator):
-    """This function return the mean and std randomly selected from the duration given using the random_generator passed. These params are required to obtain a new base duration or the job risks.
-    Args:
-        duration (Integer): A value specifying the duration to change.
-        random_generator (scipy.stats.norm): Instance of scipy.stats.norm object to generate random values of the normal distribution.
-    """ 
-    mean = random_generator.rvs() * duration
-    std = abs(random_generator.rvs() * duration)
-    return mean, std
-
-def get_new_job_base_duration(duration, mean, std):
+def get_new_job_base_duration(duration):
     """This function return a new randomly duration for the duration given using the mean and std. If the duration is 1 it will add always more time.
     Args:
         duration (Integer): A value specifying the duration to change.
-        mean (Float): The mean of the normal distribution to use for the probabilities callback.
-        std (Float): The std of the normal distribution to use for the probabilities callback.
     """ 
-    probability = __get_dist_prob__(mean, std)
-    new_duration = duration
-    if probability() >= 0.5: # Duration modified
-        if probability() > 0.5:
-            new_duration *= 1 + probability() # Duration increased
-        else:
-            new_duration *= probability() # Duration decreased
-    if duration == 1 and new_duration < 1.:
-        return 1 + np.ceil(new_duration)
-    else:
-        return np.ceil(new_duration)
+    return stats.norm.rvs(loc=duration, scale=duration * 0.3)
 
-def get_job_risks(risks_per_job, mean, std):
+def get_job_risks(risks_per_job, base_duration):
     """This function will return a dictionary with the risk percentages and the new duration of the task with the risk applied. The dict will have the following structure:
     {
         "risk_1": Float,
@@ -135,17 +104,16 @@ def get_job_risks(risks_per_job, mean, std):
         ...
     }
     Args:
-        base_duration (Integer): A value specifying the duration to apply risks.
         risks_per_job (Tuple): Tuple of the percentages of happening that risk and its length is the quantity of risks.
-        mean (Float): The mean of the normal distribution to use for the probabilities callback.
-        std (Float): The std of the normal distribution to use for the probabilities callback.
+        base_duration (Integer): A value specifying the duration to apply risks.
     """
     risks = {}
-    prob = __get_dist_prob__(mean, std)
+    dist = stats.norm(loc=base_duration*0.5, scale=base_duration*0.5*0.2)
+    prob = lambda: dist.cdf(dist.rvs())
     for i, p in enumerate(risks_per_job):
         risk = "risk_{}".format(i+1)
         if prob() < p:
-            risks[risk] = prob()
+            risks[risk] = dist.rvs()
         else:
             risks[risk] = None
     return risks
