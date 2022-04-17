870e142599159b6e9aea476285b75c676fca43a1
Edgar RP <jefelitman@gmail.com>
2022-04-17 17:58:37 -0500

    Main v1.2.1

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 """This file is the principal and only file that must be executed to run the program. This project contain the logic to solve with an Genetic Algorithm the MRCPSP problem. In this file is where you can edit all the hyperparameters to the algorithm to make the execution. Some other parameters as optimization rules can be edited in the schedule.py file.
 Created by: Edgar RP
-Version: 1.2
+Version: 1.2.1
 """
 
-import os
 import time
 import numpy as np
 from tqdm import tqdm
 import scipy.stats as stats
+from utils import savers as us
 from gen_alg import Genetic_Algorithm
 from utils.data_loader import load_project
 
 random_seed = None
-project = load_project("./data/j301_2")
+results_folder = "./results"
 experiment_name = "j301_2"
+project = load_project("./data/j301_2")
 if not random_seed:
     np.random.seed(project["init_random_value"])
 else:
@@ -32,31 +33,23 @@ n_mutations = 1
 n_generations = 10
 
 if __name__ == "__main__":
-    # Create the results folder where the experiments will be saved
-    results_folder = "./results"
-    if not os.path.isdir(results_folder):
-        os.mkdir(results_folder)
-    experiment_path = os.path.join(results_folder, experiment_name)
-    if not os.path.isdir(experiment_path):
-        os.mkdir(experiment_path)
-
-    start = time.time()
-    gen_alg = Genetic_Algorithm(
-            project,
-            random_generator,
-            poblation_size,
-            n_winners_x_generation,
-            n_jobs_with_risks,
-            risks_per_job,
-            n_scenarios_per_solution,
-            tolerance_x_invalid_schedule,
-        )
-    end = time.time()
-    print("The time creating the first poblation use a total of {} seconds".format(end-start))
+    exp_path = us.create_experiment_folder(results_folder, experiment_name)
     
-    progress_bar = tqdm(range(1, n_generations), desc = "Poblation makespan: {} +- {}".format(gen_alg.mean_makespan, gen_alg.std_makespan))
+    progress_bar = tqdm(range(n_generations))
     for gi in progress_bar:
-        gen_alg.evolve_poblation(parents_prob_range, n_cross_points, n_mutations, random_generator)
+        if gi == 0:
+            gen_alg = Genetic_Algorithm(
+                project,
+                random_generator,
+                poblation_size,
+                n_winners_x_generation,
+                n_jobs_with_risks,
+                risks_per_job,
+                n_scenarios_per_solution,
+                tolerance_x_invalid_schedule,
+            )
+        else:
+            gen_alg.evolve_poblation(parents_prob_range, n_cross_points, n_mutations, random_generator)
         progress_bar.set_description("Poblation makespan: {} +- {}".format(gen_alg.mean_makespan, gen_alg.std_makespan))
 
     best_solution = gen_alg.solutions[0] # Aqui cual consideramos la mejor solucion?
