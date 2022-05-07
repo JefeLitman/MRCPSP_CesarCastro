c8b988e111e04ba57ff960d4a2ae45693c9c4dc6
Edgar RP <jefelitman@gmail.com>
2022-05-07 17:13:47 +0000

    Solution v1.2

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 """This file contain the class that structure a solution for the project and estimates the base line, makespan and solution metrics as robust and quality of solution.
 Created by: Edgar RP
-Version: 1.1.2
+Version: 1.2
 """
 
 import numpy as np
@@ -9,10 +9,11 @@ import utils.metrics as um
 from schedule import Schedule
 class Solution():
     
-    def __init__(self, project, job_params, n_scenarios_sol, initial_job, final_job, tol_invalid_sch = 10):
+    def __init__(self, project, random_generator, job_params, n_scenarios_sol, initial_job, final_job, tol_invalid_sch = 10):
         """When you create a solution for the project, it generate multiples scenarios where each one have its own makespan and the same execution_line of the base line schedule but share across scenarios the risks and distribution params (mean, std) by job in all the modes. Every schedule use the priority policies to generate its own execution line.
         Args:
             project (Dict): Dictionary containing all the parameters for the project.
+            random_generator (scipy.stats.norm): Instance of scipy.stats.norm object to generate random values of the normal distribution.
             job_params (Dict): A dictionary with keys as jobs_ids and values contain the risk and distribution parameters (mean and std) for that job.
             n_scenarios_job (Int): Number of scenarios per solution, an integer indicating how many scenarios must be created using the base line to obtain the mean makespan.
             initial_job (Int): Integer indicating the id of the initial job of the project.
@@ -51,6 +52,20 @@ class Solution():
                 valid = valid and \
                     job["renewable_resources_use"][i] <= self.renewable_resources_total[i]
         return valid
+
+    def __format_job_params__(self, job_params):
+        """This function formats the job params to be a list of dictionaries instead of a dictionary of dictionary, also, add the job id and mode to the dict. Return the job_params formatted.
+        Args:
+            job_params (Dict): A dictionary with keys as jobs_ids and values contain the risk and distribution parameters (mean and std) for that job.
+        """
+        job_list = []
+        for job_str in job_params:
+            job_list.append(job_params[job_str])
+            job_id, job_mode = [int(i) for i in job_str.split(".")]
+            job_list[-1]["id"] = job_id
+            job_list[-1]["mode"] = job_mode
+
+        return job_list
 
     def __create_valid_base_line__(self, project, job_params, exec_line = None):
         """This function create a valid base_line given the project data, job parameters, the tolerance for invalid schedules and an execution line to use. It returns nothing but set the base_line parameter in the object.
@@ -102,3 +117,5 @@ class Solution():
         self.std_makespan = np.std(makespans)
         self.robustness = um.get_solution_robustness(self.base_line, self.scenarios)
         self.quality = um.get_quality_robustness(self.base_line, self.scenarios)
+
+# En este archivo deberia hacer el secuenciador de la solucion 
