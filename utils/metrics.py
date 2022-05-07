ec0905219b330f9451a0f0dfce8fe4d71ad95a7a
Edgar RP <jefelitman@gmail.com>
2022-05-07 17:13:13 +0000

    Utils metrics v1.1.1

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,11 +1,11 @@
 """This file contains code to calculate the metrics for solutions in the MRCPSP problem.
 Created by: Edgar RP
-Version: 1.1
+Version: 1.1.1
 """
 
 import numpy as np
 
-def __check_similirity(base_line, scenarios):
+def __check_similirity__(base_line, scenarios):
     """This function check if each scenario have the same execution_line as the base_line. It doesn't return nothing but instead raise a value error.
     Args:
         base_line (Schedule): Instance of the class schedule that contains and execution_line and a time_line.
@@ -21,7 +21,7 @@ def get_solution_robustness(base_line, scenarios):
         base_line (Schedule): Instance of the class schedule that contains and execution_line and a time_line.
         scenarios (List[Schedule]): A list of instances of the class schedule that must contain the exact same execution_line but different time_line of the base_line.
     """
-    __check_similirity(base_line, scenarios)
+    __check_similirity__(base_line, scenarios)
     SR_jobs = np.zeros_like(base_line.time_line)
     base_times = np.r_[base_line.time_line]
     for sc in scenarios:
@@ -35,9 +35,9 @@ def get_quality_robustness(base_line, scenarios):
         base_line (Schedule): Instance of the class schedule that contains and execution_line and a time_line.
         scenarios (List[Schedule]): A list of instances of the class schedule that must contain the exact same execution_line but different time_line of the base_line.
     """
-    __check_similirity(base_line, scenarios)
+    __check_similirity__(base_line, scenarios)
     QR = 0
-    mean_makespan = np.mean([i.time_line[-1] for i in scenarios])
+    mean_makespan = np.mean([i.time_line[-1] for i in scenarios]) # Aqui tengo que usar ahora si la linea base
     for sc in scenarios:
         QR += np.abs(mean_makespan - sc.time_line[-1])
     return QR / len(scenarios)
