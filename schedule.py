5547cc32dd694f2f5ff187a067bc02a96b4ac589
Edgar RP <jefelitman@gmail.com>
2022-05-09 02:28:13 +0000

    Schedule v1.3.1

--- schedule.py
+++ schedule.py
@@ -1,6 +1,6 @@
 """This file contain the class that structure a schedule for the solution and generate the execution line, time line and duration of jobs to do.
 Created by: Edgar RP
-Version: 1.3
+Version: 1.3.1
 """
 
 import numpy as np
@@ -8,39 +8,42 @@ import utils.jobs as uj
 
 class Schedule():
     
-    def __init__(self, project, random_generator, job_params, initial_job = None, final_job = None, execution_line = None):
+    def __init__(self, project, random_generator, job_params, initial_job = None, final_job = None, base_line = None):
         """When you create a project schedule, it generates a schedule containing all the jobs in a sequential way executed in any mode and also add the random duration for risk and no risk duration.
         Args:
             project (Dict): Dictionary containing all the parameters for the project.
-            random_generator (scipy.stats.norm): Instance of scipy.stats.norm object to generate random values of the normal distribution.
+            random_generator (scipy.stats.<distribution>): Instance of scipy.stats.<distribution> object to generate random values of the normal distribution.
             job_params (List[Dict]): A list of dictionaries containing all the jobs in all the modes formatted to be used as the job_list in the schedule object.
             initial_job (Int): Integer indicating the id of the initial job of the project.
             final_job (Int): Integer indicating the id of the final job of the project.
-            execution_line (List[Str]): A optional List parameter of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be executed.
+            execution_line (Schedule): An instance of a Schedule object, which is optional if the new schedule to create must be similar to the base line.
         """
-        if execution_line == None:
-            if initial_job == None or final_job == None:
-                raise ValueError('The initial and final job must not be empty when an execution line is not given')
-            self.initial_job = initial_job
-            self.final_job = final_job
-            self.__shuffle_jobs__(job_params, random_generator)
-        else:
-            self.initial_job = int(execution_line[0].split(".")[0])
-            self.final_job = int(execution_line[-1].split(".")[0])
-        
         self.total_time = project["time_horizon"]
         self.total_jobs = project["nr_jobs"]
         self.renewable_resources_total = project["renewable_resources_total"]
         self.nonrenewable_resources_total = project["nonrenewable_resources_total"]
         self.doubly_constrained_total = project["doubly_constrained_total"]
+        self.renewable_resources_use = np.zeros_like(self.renewable_resources_total)
+        self.nonrenewable_resources_use = np.zeros_like(self.nonrenewable_resources_total)
+        self.doubly_constrained_use = np.zeros_like(self.doubly_constrained_total)
         self.job_list = job_params
-        self.__build_schedule__(execution_line)
+        if base_line == None:
+            if initial_job == None or final_job == None:
+                raise ValueError('The initial and final job must not be empty when an execution line is not given')
+            self.initial_job = initial_job
+            self.final_job = final_job
+            self.__shuffle_jobs__(job_params, random_generator)
+            #self.__build_schedule__()
+        else:
+            self.initial_job = base_line.initial_job
+            self.final_job = base_line.final_job
+            self.job_order = base_line.job_order
 
     def __shuffle_jobs__(self, job_params, random_generator):
         """This function shuffle the available jobs excepting the initial and final job for schedules created without an base schedule given. It doesn't return nothing but instead set the variable job_order.
         Args:
             job_params (List[Dict]): A list of dictionaries containing all the jobs in all the modes formatted to be used as the job_list in the schedule object.
-            random_generatorandom_generator (scipy.stats.norm): Instance of scipy.stats.norm object to generate random values of the normal distribution.
+            random_generatorandom_generator (scipy.stats.<distribution>): Instance of scipy.stats.<distribution> object to generate random values of the normal distribution.
         """
         self.job_order = ["{}.1".format(self.initial_job), "{}.1".format(self.final_job)]
         prob = lambda: random_generator.cdf(random_generator.rvs())
@@ -57,66 +60,31 @@ class Schedule():
             execution_line (List[Tuples]): A optional List parameter of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be executed. If this parameter is given then the schedule only rebuilds the time_line variable.
         """
         #First it must be executed the initial job
-        done = [(self.initial_job, 1, 0)] # (job_id, job_mode, start_tick)
-        doing = [] # (job_id, job_mode, start_tick)
-        to_do = self.__get_available_jobs__(done)
+        done = [(self.initial_job, 1, 0, 0)] # (job_id, job_mode, start_tick, job_duration)
+        doing = [] # (job_id, job_mode, start_tick, job_duration)
         time = 0
-        if execution_line == None:
-            for tick in range(self.total_time):
-                if len(to_do) != 0:
-                    job_id, job_mode = self.__select_best_job__(to_do)
-                    doing += [(job_id, job_mode, time)]
-                    to_do.pop(to_do.index((job_id, job_mode)))
-                    # aqui estoy
-                else:
-                    break
-            while time < self.total_time:
-                if len(done) < self.total_jobs - 1:
-                    job_id, job_mode = self.__select_best_job__(to_do)
-                    job = uj.get_job(self.job_list, job_id, job_mode)
-                    done += [(job_id, job_mode, time)]
-                    to_do = self.__get_available_jobs__(done)
-                    time += job["total_duration"]
-                else:
-                    done += [(self.final_job, 1, time)]
-                    time = self.total_time
-        else:
-            dependencies_problem = []
-            for job_string in execution_line[1:-1]:
-                job_id, job_mode = [int(i) for i in job_string.split(".")]
-                if (job_id, job_mode) in [(i, j) for i, j, k in to_do]:
-                    job = uj.get_job(self.job_list, job_id, job_mode)
-                    done += [(job_id, job_mode, time)]
-                    to_do = self.__get_available_jobs__(done)
-                    time += job["total_duration"]
-                else:
-                    dependencies_problem.append((job_id, job_mode))
-            
-            if len(done) < self.total_jobs - 1 and len(dependencies_problem) > 0:
-                for job_id, job_mode in dependencies_problem:
-                    job = uj.get_job(self.job_list, job_id, job_mode)
-                    done += [(job_id, job_mode, time)]
-                    time += job["total_duration"]
-            
-            done += [(self.final_job, 1, time)]
-            time = self.total_time
-            
-        self.execution_line = []
-        self.time_line = []
-        for job_id, job_mode, start_time in done:
-            self.execution_line.append("{}.{}".format(job_id, job_mode))
-            self.time_line.append(start_time)
+        for tick in range(self.total_time):
+            # Check for finished jobs
+            for i in range(len(doing)):
+                if self.__is_finished__(doing[i]):
+                    job_tuple = doing.pop(i)
+                    done.append(job_tuple)
+            # Get all the available jobs with the finished jobs
+            to_do = self.__get_available_jobs__(done)
+
+            # Iterate over all available jobs until resources are busy
+            while len(to_do) > 0:
+                pass
 
     def __get_available_jobs__(self, done_jobs):
-        """This function is in charge to return a List witht jobs ids of all  available jobs given the jobs done.
+        """This function is in charge to return an ordered List with job Dicts with the structure in utils/jobs.py following the order of job_order attribute given the jobs done.
         Args:
-            done_jobs (List[Tuple]): A list of tuples with elements (job_id, job_mode, start_tick) to get the successors ids.
+            done_jobs (List[Tuple]): A list of tuples with elements (job_id, job_mode, start_tick, job_duration) to get the successors ids.
         """
-        to_do = []
+        to_do_ids = []
         done_ids = [i[0] for i in done_jobs]
-        for job_id, job_mode , _ in done_jobs:
+        for job_id, job_mode , st, jd in done_jobs:
             job = uj.get_job(self.job_list, job_id, job_mode)
-            self.job_list['{}.{}',format(job_id, job_mode)]
             candidates = job["successors"]
             dependencies = job["predecessors"]
             for can_id in candidates:
@@ -124,37 +92,56 @@ class Schedule():
                 for dep_id in dependencies:
                     is_feasible = is_feasible and (dep_id in done_ids)
                 if is_feasible:
-                    to_do.append(can_id)
+                    to_do_ids.append(can_id)
+        
+        to_do = []
+        for job_str in self.job_order:
+            job_id, job_mode = [int(i) for i in job_str.split(".")]
+            if job_id in to_do_ids:
+                job = uj.get_job(self.job_list, job_id, job_mode)
+                to_do.append(job)
         return to_do
 
-    def __get_finished_jobs__(self, doing_jobs, actual_time):
-        pass
-
-    def __selec_random_job__(self, to_do_jobs):
-        pass
-    
-    def __select_best_mode__(self, job_id):
-        """This function apply the optimization rules to select the best following mode of the job to do and return a List with [job_id, job_mode] to be executed in the project.
+    def __is_feasible__(self, job):
+        """This function check the use of availability of resources taking into account the total and used resourced. It returns True or False if the given job can not be done at the moment.
         Args:
-            to_do_jobs (List[Tuples]): A list of tuples with elements (job_id, job_mode, predecessor_id) of all available jobs to be executed in that time.
+            job (Dict): A dictionary of a job with the structure showed in utils/jobs.py.
         """
-        jobs = []
-        optimization_rules = (
-            lambda job: job["total_duration"],
-            lambda job: np.sum(job["nonrenewable_resources_use"]),
-            lambda job: np.sum(job["renewable_resources_use"]),
-        )
-        for job_str in self.job_list:
-            if job_str.startswith("{}.".format(job_id)):
-                jobs.append(self.job_list[job_str])
+        valid = True
+        available_r_resources = np.abs(self.renewable_resources_total - self.renewable_resources_use)
+        available_nr_resources = np.abs(self.nonrenewable_resources_total - self.nonrenewable_resources_use)
+        
+        needed_r_resources = job["renewable_resources_use"]
+        needed_nr_resources = job["nonrenewable_resources_use"]
+        # Disabling the validation of non renewable resources, but the code will be stay available
+        for i in range(len(self.nonrenewable_resources_use)):
+            valid = valid and needed_nr_resources[i] <= available_nr_resources[i]
+        for i in range(len(self.renewable_resources_use)):
+            valid = valid and needed_r_resources[i] <= available_r_resources[i]
+        return valid
 
-        if len(jobs) > len(optimization_rules):
-            divisor = int(np.ceil(len(jobs) / len(optimization_rules)) - 1)
-            for rule in optimization_rules:
-                jobs = sorted(jobs, key=rule)[:-divisor]
+    def __is_finished__(self, job_tuple, time_tick):
+        """This function check if a job have finished given the time tick where the job is. It return True or False and the job_tuple must contain the following structure (job_id, job_mode, start_tick, job_duration).
+        Args:
+            job_tuple (Tuple): A tuple with elements (job_id, job_mode, start_tick, job_duration) to determine if the job have already finished.
+            time_tick (Int): An integer indicating in what time tick the job is currently.
+        """
+        job_duration = range(job_tuple[3], time_tick)
+        if len(job_duration) >= job_tuple[4]:
+            return True
         else:
-            for i in range(len(jobs) - 1):
-                jobs = sorted(jobs, key=optimization_rules[i])[:-1]
+            return False
 
-        best_job = jobs[0]
-        return best_job["id"], best_job["mode"]
+    def __can_use_more_resources__(self, to_do_jobs):
+        """This function check for every possible job if its possible to be programmed taking into account the actual use of resources. It returns True if its possible to program at least one job of the to_do_jobs list Dicts or False is there is no any job.
+        Args:
+            to_do_jobs (List[Dict]): A list of dictionaries of each job following the order of job_order attribute.
+        """
+        for job in to_do_jobs:
+            if self.__is_feasible__(job):
+                return True
+        return False
+
+    def __update_states__(self, time_tick, doing_jobs, done_jobs):
+        """This function is in construction"""
+
