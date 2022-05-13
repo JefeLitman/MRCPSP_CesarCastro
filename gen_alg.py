70daabc7998e6c72d0a266de37524f790e83cd08
Edgar RP <jefelitman@gmail.com>
2022-05-12 21:33:06 -0500

    Gen_Alg v1.4

--- gen_alg.py
+++ gen_alg.py
@@ -1,6 +1,6 @@
 """This file contain the class that execute and do the genetic algorithm of the given project with mutation, crossover and other methods neccessary.
 Created by: Edgar RP
-Version: 1.3.4
+Version: 1.4
 """
 
 import numpy as np
@@ -10,7 +10,7 @@ from solution import Solution
 
 class Genetic_Algorithm():
     
-    def __init__(self, project, random_generator, poblation_size, n_winners, n_jobs_risks, risks_per_job, n_scenarios_sol, tol_invalid_sch):
+    def __init__(self, project, random_generator, poblation_size, n_winners, n_jobs_risks, risks_per_job, n_scenarios_sol):
         """This class is the main class, it creates the quantity of solutons given, make crossover, mutation and select the best samples. This class contain important classes as to create a new generation and stablish the risks in the jobs.
         Args:
             project (Dict): Dictionary containing all the parameters for the project.
@@ -19,8 +19,7 @@ class Genetic_Algorithm():
             n_winners (Int): Quantity of solutions that will be taken into account as parents to generate the new sons. It can not be greater than poblation_size.
             n_jobs_risks (Int): Quantity of jobs which will have risks.
             risks_per_job (Tuple): Tuple of the percentages of happening that risk and its length is the quantity of risks.
-            n_scenearios_job (Int): Number of scenarios per solution, an integer indicating how many scenarios must be created using the base line to obtain the mean makespan.
-            tol_invalid_sch (Int): Tolerance of invalid schedules created, an integer indicating how many retries it can until to get a valid schedule solution.
+            n_scenarios_sol (Int): Number of scenarios per solution, an integer indicating how many scenarios must be created using the base line to obtain the mean makespan.
         """
         if poblation_size <= n_winners:
             raise AssertionError("The size of the poblation must be greater than the quantity of winners for generation. Values given {} <= {}".format(
@@ -35,12 +34,12 @@ class Genetic_Algorithm():
         self.final_job = uj.get_final_job(project["jobs"])
         self.__set_jobs_params__(project["jobs"], n_jobs_risks, risks_per_job, random_generator)
         
-        # self.solutions = []
-        # for _ in range(poblation_size):
-        #     self.solutions.append(
-        #         Solution(project, self.jobs, n_scenarios_sol, tol_invalid_sch)
-        #     )
-        # self.__sort_poblation__()
+        self.solutions = []
+        for _ in range(poblation_size):
+            self.solutions.append(
+                Solution(project, random_generator, self.jobs, n_scenarios_sol, self.initial_job, self.final_job)
+            )
+        self.__sort_poblation__()
 
     def __sort_poblation__(self):
         """This function set the metrics first in the poblation like mean and std makespan, order from lower to higher the difference in the makespan and the mean makespan and finally compute the metrics for every solution in the poblation. It doesn't return nothing."""
@@ -78,19 +77,14 @@ class Genetic_Algorithm():
             job_mode = job["mode"]
             job_key = "{}.{}".format(job_id, job_mode)
 
-            if job_id == self.initial_job:
-                self.jobs[job_key] = {"initial": True}
-            elif job_id == self.final_job:
-                self.jobs[job_key] = {"final": True}
-            else:
-                self.jobs[job_key] = {
-                    "base_duration": job["base_duration"],
-                    "normal_dist_mean": job["base_duration"],
-                    "normal_dist_std": job["base_duration"] * 0.3,
-                    "renewable_resources_use": job["renewable_resources_use"],
-                    "nonrenewable_resources_use": job["nonrenewable_resources_use"],
-                    "doubly_constrained_use": job["doubly_constrained_use"]
-                }
+            self.jobs[job_key] = {
+                "base_duration": job["base_duration"],
+                "normal_dist_mean": job["base_duration"],
+                "normal_dist_std": job["base_duration"] * 0.3,
+                "renewable_resources_use": job["renewable_resources_use"],
+                "nonrenewable_resources_use": job["nonrenewable_resources_use"],
+                "doubly_constrained_use": job["doubly_constrained_use"]
+            }
 
             for ji in job["successors"]:
                 if job_id not in dependencies[ji]:
@@ -133,7 +127,7 @@ class Genetic_Algorithm():
         for job_str in self.jobs:
             job_id = int(job_str.split(".")[0])
             if job_id not in [self.initial_job, self.final_job] + jobs_modified:
-                risks = uj.get_job_risks(np.zeros_like(risks_per), 0)
+                risks = uj.get_job_risks(np.zeros_like(risks_per), 0, random_generator)
                 for r in risks:
                     #self.jobs[job_str]["used_for_risks"] = False
                     self.jobs[job_str][r] = risks[r]
@@ -165,30 +159,32 @@ class Genetic_Algorithm():
                     if random_value >= probabilities[i]:                        
                         parents.append(winners[i])
                         break
-            exec_line_son = self.crossover_solutions(
-                parents[0].base_line.execution_line, 
-                parents[1].base_line.execution_line, 
+            job_order_son = self.crossover_solutions(
+                parents[0], 
+                parents[1], 
                 n_cross_points, 
                 random_generator
             )
+            self.solutions[index].set_job_order(job_order_son)
 
-            exec_line_son = self.mutate_solution(exec_line_son, n_mutations, random_generator)
+            exec_line_son = self.mutate_solution(self.solutions[index].execution_line, n_mutations, random_generator)
+            self.solutions[index].base_line.build_schedule(exec_line_son)
 
-            self.solutions[index].set_baseline(self.project, self.jobs, exec_line_son)
+            self.solutions[index].set_baseline(self.project, self.jobs, self.solutions[index].base_line)
 
         # After all children were created, then re sort the poblation
         self.__sort_poblation__()
 
-    def crossover_solutions(self, exec_line_0, exec_line_1, n_points, random_generator):
-        """This function is in order to create a new execution line based on the two execution lines given. It return the new execution line.
+    def crossover_solutions(self, base_line_0, base_line_1, n_points, random_generator):
+        """This function is in order to create a new job order based on the two base lines given. It return the new job order.
         Args:
-            exec_line_0 (List[Str]): A List of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be executed.
-            exec_line_1 (List[Str]): A List of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be executed.
+            base_line_0 (Schedule): An instance of a Schedule object, which is optional if the new schedule to create must be similar to the base line.
+            base_line_1 (Schedule): An instance of a Schedule object, which is optional if the new schedule to create must be similar to the base line.
             n_points (Int): An integer indicating how many points will be taking to make the crossover operation.
             random_generator (scipy.stats.<distribution>): Instance of scipy.stats.<distribution> object to generate random values of the normal distribution.
         """
-        assert len(exec_line_0) == len(exec_line_1) and len(exec_line_0) > 3
-        n_jobs = len(exec_line_1)
+        assert len(base_line_0.job_order) == len(base_line_1.job_order) and len(base_line_0.job_order) > len(self.jobs)
+        n_jobs = len(self.jobs)
         assert n_points < int(n_jobs / 2) and n_points > 0
         prob = lambda: random_generator.cdf(random_generator.rvs())
         crossover_points = [(0, int(prob()*2))] # List of tuples with (start_index, parent)
@@ -202,38 +198,37 @@ class Genetic_Algorithm():
                     crossover_points.append((index, next_parent))
         crossover_points += [(n_jobs - 1, None)]
         
-        exec_line = []
-        for i in range(len(crossover_points) - 1):
+        job_order = []
+        for i in range(len(crossover_points)):
             start_point, parent_index = crossover_points[i]
             end_point, _ = crossover_points[i + 1]
-            parent = locals()["exec_line_{}".format(parent_index)]
-            self.__add_no_repeated_jobs__(exec_line, parent, start_point, end_point - start_point)
+            parent = locals()["base_line_{}".format(parent_index)].job_order
+            self.__add_no_repeated_jobs__(job_order, parent, start_point, end_point - start_point)
 
-        exec_line.append(parent[-1]) # Add at the end the last fictional job
-        assert len(exec_line) == n_jobs
-        return exec_line
+        assert len(job_order) == n_jobs
+        return job_order
 
-    def __add_no_repeated_jobs__(self, exec_line, parent, start_index, n_add):
-        """This function add the quantity of end_index - start_jobs in the exec line using all the jobs in the parent until the quantity of jobs is accomplished. It return None but modify the exec_line element
+    def __add_no_repeated_jobs__(self, job_order, parent_job_order, start_index, n_add):
+        """This function add the quantity of end_index - start_jobs in the job order using all the jobs in the parent until the quantity of jobs is accomplished. It return None but modify the job_order element
         Args:
-            exec_line (List[Str]): A List of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be added from parent.
-            parent (List[Str]): A List of strings from the parent in the format "<job_id>.<job_mode>" that contain the order in which every job will be executed.
+            job_order (List[Str]): A List of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be added from parent.
+            parent_job_order (List[Str]): A List of strings from the parent in the format "<job_id>.<job_mode>" that contain the order in which every job will be taking into account in the sequentiator.
             start_index (Int): An integer indicating the index in which the parent jobs will start to add.
             n_add (Int): An integer indicating how many jobs will be added from the parent to exec_line.
         """
-        jobs = [int(job_string.split('.')[0]) for job_string in exec_line]
+        jobs = [int(job_string.split('.')[0]) for job_string in job_order]
         jobs_to_add = []
         index = start_index
         while len(jobs_to_add) < n_add:
-            job_id, _ = [int(i) for i in parent[index].split('.')]
+            job_id, _ = [int(i) for i in parent_job_order[index].split('.')]
             if job_id not in jobs:
-                jobs_to_add.append(parent[index])
+                jobs_to_add.append(parent_job_order[index])
                 jobs.append(job_id)
-            index = (index + 1) % (len(parent) - 1) # Exclude the last job 
-        exec_line += jobs_to_add
+            index = (index + 1) % (len(parent_job_order) - 1) # Exclude the last job 
+        job_order += jobs_to_add
 
     def mutate_solution(self, exec_line, mutations, random_generator):
-        """This function alters each job in the execution line (ignoring the first and last) changing its modes by a random probabilitie in each job. It return the new mutated execution line.
+        """This function alters each job in the execution line (ignoring the first and last) changing its modes by a random probability in each job. It return the new mutated execution line.
         Args:
             exec_line (List[Str]): A List of strings in the format "<job_id>.<job_mode>" that contain the order in which every job will be executed.
             mutations (Int): An integer indicating how many mutation tries must be executed by every job per solution. The mutations will be only to change the mode of the job.
