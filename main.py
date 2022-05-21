b4e41bc86803139d1ab7f87f2e0d8d1e066b0df6
Edgar RP <jefelitman@gmail.com>
2022-05-21 16:40:11 -0500

    Main v1.2.5

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """This file is the principal and only file that must be executed to run the program. This project contain the logic to solve with an Genetic Algorithm the MRCPSP problem. In this file is where you can edit all the hyperparameters to the algorithm to make the execution. Some other parameters as optimization rules can be edited in the schedule.py file.
 Created by: Edgar RP
-Version: 1.2.4
+Version: 1.2.5
 """
 
 import time
@@ -21,7 +21,7 @@ else:
     np.random.seed(random_seed)
 random_generator = stats.uniform(loc = 0, scale = 1)
 n_jobs_with_risks = 10
-risks_per_job = (0.05, 0.02, 0.08)
+risks_per_job = [(0.05, 0.5, 0.1), (0.02, 0.6, 0.15), (0.08, 0.3, 0.09)] # (Prob of risk, Percentage of duration for the mean, Percentage of duration for the deviation)
 n_scenarios_per_solution = 10
 n_scenarios_final = 100
 poblation_size = 10
@@ -33,6 +33,7 @@ n_generations = 10
 
 if __name__ == "__main__":
     exp_path = us.create_experiment_folder(results_folder, experiment_name)
+    us.save_project_params(exp_path, project)
     
     progress_bar = tqdm(range(n_generations))
     for gi in progress_bar:
@@ -46,6 +47,7 @@ if __name__ == "__main__":
                 risks_per_job,
                 n_scenarios_per_solution
             )
+            us.save_job_params(exp_path, gen_alg.jobs, len(risks_per_job))
         else:
             gen_alg.evolve_poblation(parents_prob_range, n_cross_points, n_mutations, random_generator)
         progress_bar.set_description("Poblation makespan: {} +- {}".format(gen_alg.mean_makespan, gen_alg.std_makespan))
