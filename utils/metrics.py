e20ec4a343d9c5d613279df95c1590fd4b965caf
Edgar RP <jefelitman@gmail.com>
2022-05-21 16:38:55 -0500

    Utils metrics v1.3

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,6 +1,6 @@
 """This file contains code to calculate the metrics for solutions in the MRCPSP problem.
 Created by: Edgar RP
-Version: 1.2
+Version: 1.3
 """
 
 import numpy as np
@@ -12,8 +12,8 @@ def __check_similirity__(base_line, scenarios):
         scenarios (List[Schedule]): A list of instances of the class schedule that must contain the exact same execution_line but different time_line of the base_line.
     """
     for sc in scenarios:
-        if sc.execution_line != base_line.execution_line:
-            raise AssertionError("A scenario from the base line given is different in the execution line, this means that a scenario is for another solution than the given.")
+        if sc.job_order != base_line.job_order:
+            raise AssertionError("A scenario from the base line given is different in the job order, this means that a scenario is for another solution than the given.")
 
 def get_solution_robustness(base_line, scenarios):
     """This function return the solution robustenss value (SR) given the base line and simulated scenarios from that solution, each scenario must have the same execution_line but different time lines.
@@ -22,7 +22,7 @@ def get_solution_robustness(base_line, scenarios):
         scenarios (List[Schedule]): A list of instances of the class schedule that must contain the exact same execution_line but different time_line of the base_line.
     """
     __check_similirity__(base_line, scenarios)
-    SR_jobs = np.zeros_like(base_line.time_line)
+    SR_jobs = np.zeros_like(base_line.time_line) # Corregir esto porque el execution line no es igual
     base_times = np.r_[base_line.time_line]
     for sc in scenarios:
         SR_jobs += np.abs(base_times -  sc.time_line)
